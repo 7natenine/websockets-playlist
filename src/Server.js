3b6ecac469562aeb029dc221d2aec7dba11637f8
Nathan Leung <nath.leung@gmail.com>
2020-05-07 18:04:55 -0700

    update

--- src/Server.js
+++ src/Server.js
@@ -1,9 +1,12 @@
+import openSocket from 'socket.io-client';
+
 const { PORT } = require('./config')
+const socket = openSocket(`http://localhost:${PORT}`);
 const App = require('./App')
 
 
-App.listen(PORT, () => { 
-  console.log(`Server listening at http://localhost:${PORT}`)
+App.listen(socket, () => { 
+  console.log(`Server listening at ${socket}`)
 });
 
 module.exports = {App};
